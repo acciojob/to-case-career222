42f17918f6f279f73b4bef81062f3f1a217b611d
career222 <gadedipak1111@gmail.com>
2026-09-05 20:19:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function toCase(text) {
-  // write your code here
+function toCase(str) {
+  return str.toLowerCase() + "-" + str.toUpperCase();
 }
-
-// DO not change the code below
-
-const text = prompt("Enter text:");
-alert(toCase(text));
